71776624e2d4a19c489692cad75a53c00391f494
David <55225273+DavidvanEersel@users.noreply.github.com>
2020-05-22 11:54:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route('/home')
+def hello_world():
+    return render_template("home.html")
+
+
+@app.route("/blast", methods=["POST", "GET"])
+def blast():
+    seq = request.form.get("seq", '')
+    return render_template("blast.html")
+
+
+@app.route("/organisme")
+def organisme():
+    return render_template("organisme.html")
+
+
+@app.route("/overons")
+def overons():
+    return render_template("over ons.html")
+
+
+@app.route("/proteines")
+def proteines():
+    return render_template("proteines.html")
 
 
 if __name__ == '__main__':
