dedea6fd12e3eae942517ba4e6d040023048f449
David <55225273+DavidvanEersel@users.noreply.github.com>
2020-05-31 10:04:42 +0200

    Add files via upload

---
+++ inlezen_xml.py
@@ -0,0 +1,113 @@
+##########################################################################
+# Auteur: David van Eersel
+# Datum 5-5-20
+# Functie: het inlezen van een xml bestand
+# versie: 1.0 : het inlezen van een txt output bestand van blast
+# versie: 2.0 : het inlezen van een xml output bestand van blast
+# versie: 2.1 : extra parameters toegevoegd
+##########################################################################
+
+
+def lees_bestand():
+    try:
+        data = list()
+        for i in open("my_blast.xml"):
+            data.append(i.strip())
+        return data
+    except ValueError:
+        print("Geen alignments")
+
+
+class Reader:
+    def __init__(self):
+        self.a_name = list()
+        self.list = list()
+        self.accesie_code = list()
+        self.max_score = list()
+        self.identities = list()
+        self.frame = list()
+        self.e_value = list()
+        self.positives = list()
+        self.data = lees_bestand()
+
+    def get_accessie_code(self):
+        return self.set_accessie_code()
+
+    def get_max_score(self):
+        return self.set_max_score()
+
+    def get_identities(self):
+        return self.set_identities()
+
+    def get_frame(self):
+        return self.set_frame()
+
+    def get_e_value(self):
+        return self.set_e_value()
+
+    def get_positives(self):
+        return self.set_positives()
+
+    def get_a_name(self):
+        return self.set_a_name()
+
+    def zoek(self, variable):
+        self.list.clear()
+        counter = 1
+        for i in range(len(self.data)):
+            if '<?xml version="1.0"?>' in self.data[i]:
+                self.list.append("Blast" + str(counter))
+                counter += 1
+                counter2 = 1
+                for j in range(1, len(self.data)):
+                    if counter2 <= 20:
+                        if variable in self.data[j]:
+                            counter2 += 1
+                            self.list.append(
+                                self.data[j].replace("<" + variable + ">", "").replace("</" + variable + ">", ''))
+                        elif '<?xml version="1.0"?>' in self.data[j]:
+                            break
+                    else:
+                        break
+            else:
+                pass
+        return self.list
+
+    def set_accessie_code(self):
+        tussenstap = self.zoek("Hit_accession")
+        self.accesie_code = tussenstap.copy()
+        return self.accesie_code
+
+    def set_max_score(self):
+        tussenstap = self.zoek("Hsp_score")
+        self.max_score = tussenstap.copy()
+        return self.max_score
+
+    def set_identities(self):
+        tussenstap = self.zoek("Hsp_identity")
+        self.identities = tussenstap.copy()
+        return self.identities
+
+    def set_frame(self):
+        tussenstap = self.zoek("Hsp_query-frame")
+        self.frame = tussenstap.copy()
+        return self.frame
+
+    def set_e_value(self):
+        tussenstap = self.zoek("Hsp_evalue")
+        self.e_value = tussenstap.copy()
+        return self.e_value
+
+    def set_positives(self):
+        tussenstap = self.zoek("Hsp_positive")
+        self.positives = tussenstap.copy()
+        return self.positives
+
+    def set_a_name(self):
+        tussenstap = self.zoek("Hit_def")
+        self.a_name = tussenstap.copy()
+        return self.a_name
+
+
+if __name__ == '__main__':
+    reader = Reader()
---
+++ sql_biopython.py
@@ -0,0 +1,66 @@
+##########################################################################
+# Auteur: David van Eersel
+# Datum 5-13-20
+# Functie: het blasten van de sequenties met blastx tegen de nr database.
+#          Daarbij worden de sequenties uit de database gehaald
+# versie: 2.0
+# (versie 1.0 was het vorige biopythonscript)
+##########################################################################
+
+from Bio.Blast import NCBIWWW
+import mysql.connector
+import time
+
+
+
+def getal():
+    """
+    hier wordt er verbinding gemaakt met de database. er wordt dan geteld hoeveel sequenties in de datbase staan om
+    deze later weer te kunnen gebruiken in een for loop
+    :return: het aantal sequenties die in de database staan
+    """
+    conn = mysql.connector.connect(
+        host="hannl-hlo-bioinformatica-mysqlsrv.mysql.database.azure.com",
+        user="piion@hannl-hlo-bioinformatica-mysqlsrv", db="piion",
+        password='637476')
+
+    cursur = conn.cursor()
+    cursur.execute("select count(sequentie) from sequences")
+    getal = cursur.fetchall()
+    cursur.close()
+    conn.close()
+    return getal
+
+
+def blast(getal):
+    """ hier worden de de sequenties geblast. deze worden opgehaald uit de database. dit wordt gedaan met de id nummers
+    :param getal: aantal sequenties
+    :return: een bestand waar de blast resultaten inzitten
+    """
+    #with open("my_blast.xml", "w") as out_handle:  # maakt het bestand leeg als hier nog wat in staat
+        #out_handle.write("")
+    idn = 186  # het id nummer
+    for i in range(185, int(getal[0][0])):  # de connectie wordt elke keer verbroken omdat het programma anders te lange
+        # tijd zonder wat te doen verbonden is aan de database
+        idn += 1
+        seq = list()
+        conn = mysql.connector.connect(
+            host="hannl-hlo-bioinformatica-mysqlsrv.mysql.database.azure.com",
+            user="piion@hannl-hlo-bioinformatica-mysqlsrv", db="piion",
+            password='637476')
+        cursur = conn.cursor()
+        cursur.execute("select sequentie from sequences where id =" + str(idn) + ";")
+        rows = cursur.fetchall()
+        seq.append(rows[0][0])
+        cursur.close()
+        conn.close()
+        print(seq[0])
+        blast = NCBIWWW.qblast('blastx', 'nr', seq[0])
+        with open("my_blast.xml", "a") as out_handle:
+            out_handle.write(blast.read())
+        time.sleep(600)  # seconde
+
+
+if __name__ == '__main__':
+    g1 = getal()
+    blast(g1)
