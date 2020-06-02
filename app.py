a4a5c109f05ebba528f620d4f8bcfa2a1e37f343
PieterVerhoef <pieter.verhoef@upcmail.nl>
2020-06-02 15:32:47 +0200

    added chances

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/home')
+@app.route('/')
 def hello_world():
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 @app.route("/blast", methods=["POST", "GET"])
 def blast():
     seq = request.form.get("seq", '')
-    return render_template("blast.html")
+    pagenummer = int(request.args.get('page', "0"))
+    return render_template("blast.html", pagenummer=pagenummer)
 
 
 @app.route("/organisme")
@@ -21,10 +22,10 @@ def organisme():
 
 @app.route("/overons")
 def overons():
-    return render_template("over ons.html")
+    return render_template("overons.html")
 
 
-@app.route("/proteines")
+@app.route("/proteine")
 def proteines():
     return render_template("proteines.html")
 
